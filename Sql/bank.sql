9dd1fbd860ac844efe7f50f25e79acaf652d0b5d
mahaocong <784466409@qq.com>
2018-11-27 23:14:57 +0800

    Add bank.sql

---
+++ Sql/bank.sql
@@ -0,0 +1,150 @@
+/*
+ Navicat MySQL Data Transfer
+
+ Source Server         : mysql_1
+ Source Server Type    : MySQL
+ Source Server Version : 50642
+ Source Host           : localhost:3306
+ Source Schema         : bank
+
+ Target Server Type    : MySQL
+ Target Server Version : 50642
+ File Encoding         : 65001
+
+ Date: 26/11/2018 17:45:56
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for account_sums
+-- ----------------------------
+DROP TABLE IF EXISTS `account_sums`;
+CREATE TABLE `account_sums`  (
+  `account_id` varchar(19) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `account_balance` decimal(19, 2) NULL DEFAULT NULL,
+  `account_psw` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `site_id` int(11) NULL DEFAULT NULL,
+  `u_id` varchar(18) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`account_id`) USING BTREE
+) ENGINE = MyISAM CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for accounts
+-- ----------------------------
+DROP TABLE IF EXISTS `accounts`;
+CREATE TABLE `accounts`  (
+  `acconut_id` varchar(19) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `account_balance` decimal(19, 2) NULL DEFAULT NULL,
+  `password` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `site_id` int(11) NULL DEFAULT NULL,
+  `user_id` varchar(18) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`acconut_id`) USING BTREE
+) ENGINE = MyISAM CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for deposits
+-- ----------------------------
+DROP TABLE IF EXISTS `deposits`;
+CREATE TABLE `deposits`  (
+  `deposits_id` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `account_balance` decimal(19, 2) NULL DEFAULT NULL,
+  `account_id` varchar(19) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `deposits_date_time` datetime(0) NULL DEFAULT NULL,
+  `deposits_value` decimal(19, 2) NULL DEFAULT NULL,
+  `site_id` int(11) NULL DEFAULT NULL,
+  `staff_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`deposits_id`) USING BTREE
+) ENGINE = MyISAM CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for hibernate_sequence
+-- ----------------------------
+DROP TABLE IF EXISTS `hibernate_sequence`;
+CREATE TABLE `hibernate_sequence`  (
+  `next_val` bigint(20) NULL DEFAULT NULL
+) ENGINE = MyISAM CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Fixed;
+
+-- ----------------------------
+-- Table structure for sites
+-- ----------------------------
+DROP TABLE IF EXISTS `sites`;
+CREATE TABLE `sites`  (
+  `site_id` int(11) NOT NULL,
+  `site_address` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `site_name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`site_id`) USING BTREE
+) ENGINE = MyISAM CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for staffs
+-- ----------------------------
+DROP TABLE IF EXISTS `staffs`;
+CREATE TABLE `staffs`  (
+  `staff_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `staff_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `staff_phone` varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `staff_site` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`staff_id`) USING BTREE
+) ENGINE = MyISAM CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for summarys
+-- ----------------------------
+DROP TABLE IF EXISTS `summarys`;
+CREATE TABLE `summarys`  (
+  `summary_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `account_id_from` varchar(19) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `account_id_to` varchar(19) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `date` datetime(0) NULL DEFAULT NULL,
+  `site_id` int(11) NULL DEFAULT NULL,
+  `type` int(11) NULL DEFAULT NULL,
+  `value` decimal(19, 2) NULL DEFAULT NULL,
+  PRIMARY KEY (`summary_id`) USING BTREE
+) ENGINE = MyISAM CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for transfers
+-- ----------------------------
+DROP TABLE IF EXISTS `transfers`;
+CREATE TABLE `transfers`  (
+  `transfer_id` int(11) NOT NULL AUTO_INCREMENT,
+  `account_from_balance` decimal(19, 2) NULL DEFAULT NULL,
+  `account_id_from` varchar(19) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `account_id_to` varchar(19) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `account_to_balance` decimal(19, 2) NULL DEFAULT NULL,
+  `site_id` int(11) NULL DEFAULT NULL,
+  `staff_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `transfer_date` datetime(0) NULL DEFAULT NULL,
+  `transfer_value` decimal(19, 2) NULL DEFAULT NULL,
+  PRIMARY KEY (`transfer_id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`  (
+  `u_id` varchar(18) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `u_name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `uphone` varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`u_id`) USING BTREE
+) ENGINE = MyISAM CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for withdrawals
+-- ----------------------------
+DROP TABLE IF EXISTS `withdrawals`;
+CREATE TABLE `withdrawals`  (
+  `withdrawals_id` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `accountid` varchar(19) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `account_balance` decimal(19, 2) NULL DEFAULT NULL,
+  `site_id` int(11) NULL DEFAULT NULL,
+  `staff_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `with_date` datetime(0) NULL DEFAULT NULL,
+  `withdrawalsvalue` decimal(19, 2) NULL DEFAULT NULL,
+  PRIMARY KEY (`withdrawals_id`) USING BTREE
+) ENGINE = MyISAM CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
